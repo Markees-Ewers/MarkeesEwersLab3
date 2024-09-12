a6146b519d5af6a8ff0bd0487474f9b24321d228
Markees Anthony Ewers <me00070@my.westga.edu>
2024-09-12 02:40:43 -0400

    Added the Change Grade button and updated UI
    updated the UI to include the change grade button and added the grade appended on to the display

--- code/CourseManagementSystem/src/edu/westga/cs1302/cms/model/Student.java
+++ code/CourseManagementSystem/src/edu/westga/cs1302/cms/model/Student.java
@@ -1,23 +1,29 @@
 package edu.westga.cs1302.cms.model;
 
-/** Stores and manages information for a single student.
+// TODO: Auto-generated Javadoc
+/**
+ * Stores and manages information for a single student.
  * 
  * @author CS 1302
  * @version Fall 2024
  */
 public class Student {
-	private String name;
-	private int grade;
 	
-	/** Create a new student with the specified name
-	 * 
-	 * @precondition name != null && name.length() >= 3
-	 * @postcondition getName() == name
-	 * 
-	 * @param name the name of the new student
+	/** The name. */
+	private String name;
+	
+	/** The grade. */
+	private int grade;
+
+	/**
+	 * Create a new student with the specified name.
+	 *
+	 * @param name  the name of the new student
 	 * @param grade the grade for the new student
-	 * 
 	 * @throws IllegalArgumentException when precondition is violated
+	 * @precondition name != null && name.length() >= 3
+	 * @precondtion grade !> 100 || grade < 0
+	 * @postcondition getName() == name
 	 */
 	public Student(String name, int grade) throws IllegalArgumentException {
 		if (name == null) {
@@ -26,29 +32,51 @@ public class Student {
 		if (name.length() < 3) {
 			throw new IllegalArgumentException("Name must have at least 3 characters.");
 		}
+		if (grade > 100 || grade < 0) {
+			throw new IllegalArgumentException("Grade must be between 0 and 100");
+		}
 		this.name = name;
 		this.grade = grade;
 	}
-	
-	/** Return the name of the student
-	 * 
+
+	/**
+	 * Return the name of the student.
+	 *
 	 * @return the name of the student
 	 */
 	public String getName() {
 		return this.name;
 	}
-	
-	/** Returns the grade for the student
-	 * 
+
+	/**
+	 * Returns the grade for the student.
+	 *
 	 * @return the grade for the student
 	 */
 	public int getGrade() {
 		return this.grade;
 	}
-	
+
+	/**
+	 * Sets the grade.
+	 *
+	 * @param grade the new grade
+	 */
+	public void setGrade(int grade) {
+		if (grade < 0 || grade > 100) {
+			throw new IllegalArgumentException("Grade must be between 0 and 100");
+		}
+		this.grade = grade;
+	}
+
+	/**
+	 * To string.
+	 *
+	 * @return the string
+	 */
 	@Override
 	public String toString() {
-		return this.name;
+		return this.name + " :" + this.grade;
 	}
-	
+
 }
--- code/CourseManagementSystem/src/edu/westga/cs1302/cms/view/MainWindow.java
+++ code/CourseManagementSystem/src/edu/westga/cs1302/cms/view/MainWindow.java
@@ -4,6 +4,7 @@ import edu.westga.cs1302.cms.model.Student;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
@@ -22,21 +23,28 @@ public class MainWindow {
 	private ListView<Student> students;
 
 	@FXML
+	private TextField gradeTextField;
+	@FXML
+	private Button changeGradeButton;
+
+	@FXML
 	void addStudent(ActionEvent event) {
 		String studentName = this.name.getText();
 		try {
 			int grade = Integer.parseInt(this.grade.getText());
 			Student student = new Student(studentName, grade);
 			this.students.getItems().add(student);
+			this.name.clear();
+			this.grade.clear();
 		} catch (NumberFormatException errorThing) {
 			Alert errorPopup = new Alert(Alert.AlertType.ERROR);
 			errorPopup.setContentText(
-					"Unable to create student: " + errorThing.getMessage() + ". Please reenter grade and try again.");
+					"Unable to create student: " + errorThing.getMessage() + ". Please re-enter grade and try again.");
 			errorPopup.showAndWait();
 		} catch (IllegalArgumentException errorObject) {
 			Alert errorPopup = new Alert(Alert.AlertType.ERROR);
 			errorPopup.setContentText(
-					"Unable to create student: " + errorObject.getMessage() + ". Please reenter name and try again.");
+					"Unable to create student: " + errorObject.getMessage() + ". Please re-enter name and try again.");
 			errorPopup.showAndWait();
 		}
 	}
@@ -54,10 +62,45 @@ public class MainWindow {
 	}
 
 	@FXML
-	void initialize() {
-		assert this.name != null : "fx:id=\"name\" was not injected: check your FXML file 'MainWindow.fxml'.";
-		assert this.students != null : "fx:id=\"students\" was not injected: check your FXML file 'MainWindow.fxml'.";
+	void changeGrade(ActionEvent event) {
+	    Student selectedStudent = this.students.getSelectionModel().getSelectedItem();
+	    if (selectedStudent != null) {
+	        try {
+	            int newGrade = Integer.parseInt(this.gradeTextField.getText());
+	            selectedStudent.setGrade(newGrade);
+	            this.gradeTextField.clear();
+	            this.students.refresh();
+	        } catch (NumberFormatException ex) {
+	            Alert errorPopup = new Alert(Alert.AlertType.ERROR);
+	            errorPopup.setContentText("Invalid grade. Please enter a valid number.");
+	            errorPopup.showAndWait();
+	            this.grade.clear();
+	        } catch (IllegalArgumentException go) {
+	        	Alert errorPopup = new Alert(Alert.AlertType.ERROR);
+	            errorPopup.setContentText(go.getMessage());
+	            errorPopup.showAndWait();
+	            this.grade.clear();
+	        }
+	    }
+	}
 
+    @FXML
+    void initialize() {
+    	 assert this.changeGradeButton != null : "fx:id=\"changeGradeButton\" was not injected: check your FXML file 'MainWindow.fxml'.";
+         assert this.grade != null : "fx:id=\"grade\" was not injected: check your FXML file 'MainWindow.fxml'.";
+         assert this.gradeTextField != null : "fx:id=\"gradeTextField\" was not injected: check your FXML file 'MainWindow.fxml'.";
+         assert this.name != null : "fx:id=\"name\" was not injected: check your FXML file 'MainWindow.fxml'.";
+         assert this.students != null : "fx:id=\"students\" was not injected: check your FXML file 'MainWindow.fxml'.";
+         this.gradeTextField.setEditable(false);
+
+        this.students.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {  
+             this.gradeTextField.setEditable(true);
+            } else {
+             this.gradeTextField.setEditable(false);
+            }
+        });
+		
 	}
 
 }
